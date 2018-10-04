45c0d8d12f14c9422f1696b213fb0a567bc0fa6a
Casey Shamey <caseyshamey@gmail.com>
2018-10-04 17:47:58 -0400

    Complete sum all number in array of nested arrays

--- src/recursion.js
+++ src/recursion.js
@@ -7,17 +7,51 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  }
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+//   if (array.length === 0) {
+//     return 0;
+//   }
+//   if (array.length === 1) {
+//     return array[0];
+//   } else {
+//     return array[array.length - 1] + sum(array.slice(0, -1));
+//   }
+// };
+  if (array.length === 0) {
+    return 0;
+  } else {
+    return array[0] + sum(array.slice(1));
+  }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var sum = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] instanceof Array) {
+      sum += arraySum(array[i])
+    } else {
+      sum += array[i]
+    }
+  }
+  return sum;
 };
+
+//check if current element is number or array
+
 
 // 4. Check if a number is even.
 var isEven = function(n) {
